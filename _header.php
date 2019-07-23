3f2f5faaf5af22619609d3d1d2133252468d4970
Georgia Bains <georgia.bains@volarisgroup.com>
2019-07-23 16:49:37 -0400

    Initial commit.

---
+++ _header.php
@@ -0,0 +1,15 @@
+<header>
+
+    <h1 class="main-title">Georgia Bains</h1>
+    
+    <nav>
+        <ul>
+            <li><a href="about.php">About</a></li>
+            <li><a href="projects.php">Projects</a></li>
+            <li><a title="link to resume">Resume</a></li>
+            <li><a href="https://github.com/georgiabains">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/georgia-bains/">LinkedIn</a></li>
+        </ul>
+    </nav>
+
+</header>
---
+++ projects.php
@@ -0,0 +1,156 @@
+<?php include "_top.php";?>
+<?php include "_header.php";?>
+
+<section id="list-of-projects">
+    <div class="project-container">
+        <div class="project-flex">
+    
+            <div class="project">
+                <div class="project-details">
+                    <img src="img/projects/volaris_one.png">
+                    <ul class="site-links">
+                        <li><a href="">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Volaris Group Website</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: Web Development</p>
+                    <p>Languages: PHP, HTML, CSS (SCSS), JavaScript, jQuery</p>
+                </div>
+            </div>
+            
+            <div class="project">
+                <div class="project-details">
+                    <img src="">
+                    <ul class="site-links">
+                        <li><a href="https://experientialmodules.utoronto.ca">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Experiential Learning Modules Website</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: Web Design & Development</p>
+                    <p>Languages: PHP, HTML, CSS (SCSS), JavaScript, jQuery</p>
+                </div>
+            </div>
+
+            <div class="project">
+                <div class="project-details">
+                    <img src="img/projects/aw_nuture_one.png">
+                    <ul class="site-links">
+                        <li><a href="https://www.volarisgroup.com/assetworks/assetworks-overview/">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>AssetWorks Nuture 2019 Landing Page</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: Web Development</p>
+                    <p>Languages: HTML, CSS (SCSS)</p>
+                </div>
+            </div>
+
+            <div class="project">
+                <div class="project-details">
+                    <img src="img/projects/aw_nuture_vol_one.png">
+                    <ul class="site-links">
+                        <li><a href="https://www.volarisgroup.com/assetworks/volaris-overview/">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>AssetWorks Nuture 2019 Landing Page - Volaris Branded</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: Web Development</p>
+                    <p>Languages: HTML, CSS (SCSS)</p>
+                </div>
+            </div>
+            
+            <div class="project">
+                <div class="project-details">
+                    <img src="img/projects/ocw_one.png">
+                    <ul class="site-links">
+                        <li><a href="https://ocw.utoronto.ca">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Open UToronto Website</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: Web Design & Development</p>
+                    <p>Languages: PHP, HTML, CSS, jQuery</p>
+                </div>
+            </div>
+            
+            <div class="project">
+                <div class="project-details">
+                    <img src="">
+                    <ul class="site-links">
+                        <li><a href="">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Spotlight - Tumblr Theme</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: Web Design & Development</p>
+                    <p>Languages: HTML, CSS, jQuery</p>
+                </div>
+            </div>
+            
+            <div class="project">
+                <div class="project-details">
+                    <img src="">
+                    <ul class="site-links">
+                        <li><a href="">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Project title</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: role here</p>
+                    <p>Languages: CSS, HTML etc.</p>
+                </div>
+            </div>
+            
+            <div class="project">
+                <div class="project-details">
+                    <img src="">
+                    <ul class="site-links">
+                        <li><a href="">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Project title</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: role here</p>
+                    <p>Languages: CSS, HTML etc.</p>
+                </div>
+            </div>
+            
+            <div class="project">
+                <div class="project-details">
+                    <img src="">
+                    <ul class="site-links">
+                        <li><a href="">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Project title</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: role here</p>
+                    <p>Languages: CSS, HTML etc.</p>
+                </div>
+            </div>
+            
+            
+<!--
+            <div class="project">
+                <div class="project-details">
+                    <img src="">
+                    <ul class="site-links">
+                        <li><a href="">live link</a></li>
+                        <li><a href="">view code</a></li>
+                    </ul>
+                    <h3>Project title</h3>
+                    <p>Short description of project goes here</p>
+                    <p>Role: role here</p>
+                    <p>Languages: CSS, HTML etc.</p>
+                </div>
+            </div>
+-->
+            
+        </div><!--end .project-flex-->
+    </div><!--end .project-container-->
+</section>
+
+<?php include "_footer.php";?>
+<?php include "_bottom.php";?>
