82f28daa5697f855577e6a0feaccbf7f5d4c61e3
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-02-24 15:13:18 -0500

    Project Page iniital commit.

---
+++ projects-page.php
@@ -0,0 +1,15 @@
+<?php include "_top.php";?>
+<?php include "_header.php";?>
+
+<main>
+
+	<section>
+
+		<h2>Project title</h2>
+
+	</section>
+
+</main>
+
+<?php include "_footer.php";?>
+<?php include "_bottom.php";?>
