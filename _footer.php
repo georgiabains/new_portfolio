0b6058921921929650bba2932e49e16fe52fb12c
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-02-24 14:46:18 -0500

    Footer styling. Added a section that is only sometimes visible.

--- _footer.php
+++ _footer.php
@@ -1,5 +1,16 @@
 <footer>
 
-    &copy; Georgia Bains 2020
+	<div class="footer-content">
+		<div class="footer-copyright">
+    		<p>&copy; Georgia Bains 2020</p>
+    	</div>
+    	<div class="footer-on-scroll">
+    		<ul>
+    			<li>georgiabains@hotmail.co.uk</li>
+    			<li><a href="">github</a></li>
+    			<li><a href="">linkedin</a></li>
+    		</ul>
+    	</div>
+    </div>
 
 </footer>
