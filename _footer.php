5e965595a6aeaebf23a7c2a0ccec20a904489c2a
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-02-24 15:04:07 -0500

    Small edits to footer styling and content.

--- _footer.php
+++ _footer.php
@@ -7,8 +7,8 @@
     	<div class="footer-on-scroll">
     		<ul>
     			<li>georgiabains@hotmail.co.uk</li>
-    			<li><a href="">github</a></li>
-    			<li><a href="">linkedin</a></li>
+    			<li><a href="https://github.com/georgiabains" target="_blank">github</a></li>
+    			<li><a href="https://www.linkedin.com/in/georgia-bains/" target="_blank">linkedin</a></li>
     		</ul>
     	</div>
     </div>
