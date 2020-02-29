4b429870402508f3afe8ea6ca845043e78aa73cc
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-02-28 23:54:17 -0500

    Edits to project details page. Cleaned up js folders and file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,15 @@
 var hideNav = false;
 
 $(document).ready(function () {
-    (function blink() { 
-      $('.blinking-underscore').fadeOut(750).fadeIn(750, blink); 
-    })(); 
+    // (function blink() { 
+    //   $('.blinking-underscore').fadeOut(750).fadeIn(750, blink); 
+    // })(); 
 
 	$("#menu").click(function() {
         $("#navigation").toggleClass("nav-on-click");
         $("#navigation").fadeToggle();
         hideNav = false;
     });
-
-    // test();
-
-    typeWriter("test");
-
 });
 
 $(document).scroll(function() {
--- projects-page.php
+++ projects-page.php
@@ -3,14 +3,22 @@
 
 <main>
 
-	<section>
+	<section class="project-page-details">
 
 		<h2>Volaris Group</h2>
-		<h3>Core Website</h3>
+		<h3>> core website<span class="blinking-underscore">_</span></h3>
 
 		<img src="img/projects/volaris/volaris_mockup_one.png">
 
 		<p>The core website for the company Volaris Group, a company dedicated to acquiring and supporting its child companies.</p>
+
+		<img src="img/projects/volaris/volaris_mockup_four.png">
+
+		<img src="img/projects/volaris/volaris_mockup_two.png">
+
+		<img src="img/projects/volaris/volaris_mockup_three.png">
+
+
 
 	</section>
 
