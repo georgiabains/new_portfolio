cecd99bbdafe764fdc04d54f5aef12c89e637c5c
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-03-20 10:52:57 -0400

    Changed link colour, added top half-border to project descriptions.

--- _top.php
+++ _top.php
@@ -8,6 +8,7 @@
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     
         <link rel="stylesheet" href="css/styles.css?<?php echo time(); ?>">
+
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     
     </head>
