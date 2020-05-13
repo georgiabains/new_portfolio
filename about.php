8e7678f5d4003df32f11d32557d0aee2f9125364
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-05-13 16:09:54 -0400

    Updated About copy.

--- about.php
+++ about.php
@@ -7,9 +7,8 @@
 		<h2>About</h2>
 
 		<div class="about-content">
-			<p><a href="">Lorem</a> ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat efficitur massa, in tincidunt risus consectetur id. Mauris aliquam ex sapien, eu consectetur arcu faucibus et. Praesent odio <a href="">magna</a>, faucibus at tincidunt faucibus, efficitur quis libero. Integer tempus, metus id dictum vulputate, dolor nulla dignissim dui, non malesuada lacus diam vel ipsum. <a href="">Phasellus quis velit eu</a> est fermentum porta. Aenean varius magna eu nunc dictum, eu pellentesque nibh varius. Pellentesque hendrerit pellentesque finibus. Praesent id arcu non odio gravida tempor a ac lorem. Sed facilisis lobortis tincidunt. Vivamus ac mauris vitae ante efficitur tempor. Aenean sed dui mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem ligula, bibendum nec lorem vitae, semper congue ligula. Aliquam <a href="">sapien</a> purus, pretium quis nibh sed, blandit fringilla diam.</p>
-
-			<p>Vestibulum tincidunt tortor finibus efficitur finibus. Curabitur ullamcorper lacinia vestibulum. Donec lacinia accumsan purus, eu maximus tortor fermentum et. Sed ex lacus, lacinia nec tristique non, varius quis felis. Morbi faucibus elit lectus, ullamcorper iaculis justo lacinia sed. Pellentesque ultricies magna quis iaculis posuere. Cras non ante sapien.</p>
+			<p>My name is Georgia Bains and I've been designing & developing websites since 2014. I taught myself the basics of coding while creating themes for the platform Tumblr, and I've since extended those skills into developing WordPress themes, static pages, and sites on ExpressionEngine. I focus on accessible and responsive design and development, so that anyone can access your website from anywhere.</p>
+			<p>View my full <a>resumé</a>, or contact me via email <a href="mailto:georgiabains@hotmail.co.uk">email</a>
 		</div>
 
 	</section>
