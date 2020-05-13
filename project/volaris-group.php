89c9c02fe841441e34bf1db03ced3f30a6086548
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-05-13 16:51:58 -0400

    Updated case study copy for Volaris Group.

--- project/volaris-group.php
+++ project/volaris-group.php
@@ -23,35 +23,49 @@
 		<h2>Volaris Group</h2>
 		<h3>> core website<span class="blinking-underscore">_</span></h3>
 
-		<img src="../img/projects/volaris/volaris_mockup_one.png">
+		<figure>
+			<img src="../img/projects/volaris/volaris_mockup_one.png">
+			<figcaption>A screenshot of the landing page on a MacBook, iPad, and iPhone.</figcaption>
+		</figure>
 
-		<div class="project-snippets">
-			<p>> description_</p>
-			<p>This website was created in collaboration with the Senior Web Developer and Senior Graphic Designer at Volaris Group. Duis lacinia massa ipsum, id scelerisque elit tincidunt at. Vivamus at tellus vel massa convallis luctus a nec libero. Vivamus vitae erat ultricies, accumsan tellus eget, placerat turpis. Aliquam id enim nec turpis vestibulum ullamcorper et id lacus. Nulla facilisi. Sed placerat odio rutrum, molestie mi at, eleifend nisi. Mauris euismod erat quis urna iaculis accumsan.</p>
-		</div>
 		<div class="project-snippets">
 			<p>> role_</p>
 			<p>Web Development</p>
 		</div>
 		<div class="project-snippets">
 			<p>> languages used_</p>
-			<p>CSS, (SCSS), HTML, JavaScript, jQuery & PHP</p>
+			<p>CSS, (SCSS), HTML, JavaScript, jQuery</p>
+		</div>
+		<div class="project-snippets">
+			<p>> year_</p>
+			<p>2019</p>
 		</div>
 
-		<img src="../img/projects/volaris/volaris_mockup_four.png">
+		<p>Volaris Group's core website was created in collaboration with the Senior Web Developer and Senior Graphic Designer at Volaris Group. The project involved a sizeable redesign using Volaris Group's new branding and company-specific banners.</p>
 
-		<img src="../img/projects/volaris/volaris_mockup_two.png">
+		<figure>
+			<img src="../img/projects/volaris/volaris_mockup_four.png">
+			<figcaption>A screenshot of the Owner/Operator page. The banner features an employee of Volaris Group.</figcaption>
+		</figure>
 
-		<p>Pellentesque semper urna non eros mollis imperdiet. Praesent eget sem in ligula ultricies eleifend sed fringilla tellus. Quisque at vulputate neque, non accumsan tellus. Maecenas dapibus ut erat sit amet venenatis. In egestas velit et ipsum auctor luctus. Etiam tristique nibh turpis, eget pulvinar mi pulvinar ut. Quisque lacus magna, sagittis id scelerisque at, placerat sed purus. Morbi id laoreet turpis. Mauris molestie eu erat non suscipit. Nunc non odio et sem laoreet venenatis. Cras gravida sem eu nibh venenatis aliquet. Duis quis porta libero. Maecenas placerat suscipit ipsum eu fringilla.</p>
+		<p>My contribution to this project was primarily in making the site responsive and accessible. I took great care in ensuring that any button or link that could be clicked by a mouse, could also be accessed by a keyboard. This included navigation menus and pop-up boxes.</p>
 
-		<img src="../img/projects/volaris/volaris_mockup_three.png">
+		<p>I designed the responsive breakpoints based on natural breaks in the site's content, to allow for any unsual or non-standard browser resolutions, and to create a cohesive "tablet" view, as well as the provided "mobile" view.</p>
+
+		<figure>
+			<img src="../img/projects/volaris/volaris_mockup_two.png">
+			<figcaption>Two screenshots of the site's landing page, displayed on iPhones.</figcaption>
+		</figure>
 
 		<div class="project-column">
 			<div class="column-one">
-				<img src="../img/projects/volaris/volaris_mockup_five.png">
+				<figure>
+					<img src="../img/projects/volaris/volaris_mockup_five.png">
+					<figcaption>A screenshot of the site's landing page displayed on an iPhone, with the menu expanded.</figcaption>
+				</figure>
 			</div>
 			<div class="column-two">
-				<p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed iaculis consectetur dui eget efficitur. Sed tellus neque, suscipit ut mattis sed, dictum at sapien. Nullam sollicitudin cursus risus sed euismod. In id lectus ornare lacus vestibulum sodales sit amet eget ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed elit augue, malesuada in tincidunt in, bibendum nec ex.</p>
+				<p>This screenshot demonstrates the hamburger menu in it's extended form.</p>
 			</div>
 		</div>
 
