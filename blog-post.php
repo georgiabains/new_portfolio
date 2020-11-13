fc8bb29ffb31365fbcb1e1fde35e46d501a2ba61
Georgia Bains <georgiabains@hotmail.co.uk>
2020-11-13 15:28:55 -0500

    Added styling for blog page and post page.

---
+++ blog-post.php
@@ -0,0 +1,34 @@
+<?php include "_top.php";?>
+<?php include "_header.php";?>
+
+<main>
+
+	<section class="blog">
+
+        <article class="blog-post">
+            <div class="banner">
+                <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+            </div>
+            <div class="blog-info">
+                <h2>Blog title</h2>
+                <p class="date">December 14, 2020</p>
+            </div>
+
+            <p>Donec neque tellus, facilisis a ullamcorper nec, finibus vitae sem. Phasellus tempor sit amet velit id auctor. In rutrum faucibus fringilla. Donec quis quam ut velit blandit euismod. Quisque in facilisis lorem. Nunc sit amet rhoncus neque. Donec fermentum blandit velit ac ultricies. Donec dolor purus, semper id iaculis a, imperdiet nec est. Nunc quis imperdiet augue. Mauris id augue mauris. Aenean ipsum urna, fringilla ut turpis non, condimentum eleifend diam.</p>
+
+            <code>code test</code>
+            <p>Sed dignissim lorem lorem, ut sodales lectus aliquam et. Quisque rhoncus dolor in leo ullamcorper, eget semper justo ornare. Sed eget rhoncus massa. Nunc maximus mi id sapien aliquam interdum. Pellentesque vel mollis tortor. Curabitur eget mollis eros. Vestibulum ac vulputate dui, nec varius tellus.</p>
+
+            <p>Vestibulum dictum ex a efficitur tristique. Nullam ut blandit erat. Mauris ac arcu rhoncus, finibus nibh sodales, ultricies enim. Vestibulum non maximus metus, a venenatis magna. Pellentesque nec felis et purus imperdiet accumsan eu ut ex. Nulla a mollis nulla. Ut commodo dapibus ipsum, eu facilisis ligula molestie ac. In luctus eros ipsum, ac rutrum ipsum venenatis in.</p>
+        </article>
+
+        <div class="back">
+            <a href="blog.php" class="bracket-link">back to all blog posts</a>
+        </div>
+
+	</section>
+
+</main>
+
+<?php include "_footer.php";?>
+<?php include "_bottom.php";?>
---
+++ blog.php
@@ -0,0 +1,61 @@
+<?php include "_top.php";?>
+<?php include "_header.php";?>
+
+<main>
+
+	<section class="blog">
+
+        <div class="blog-tile">
+            <div class="thumbnail">
+                <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+            </div>
+            <div class="blog-info">
+                <h3>Blog title</h3>
+                <p class="date">December 14, 2020</p>
+                <a class="bracket-link" href="blog-post.php">Read post</a>
+                <p>Phasellus augue tellus, viverra ut arcu ac, vestibulum iaculis velit. Sed eu dui consectetur, egestas est sit amet, mollis sem. Aenean rutrum quis sapien eu convallis.</p>
+            </div>
+        </div>
+
+        <div class="blog-tile">
+            <div class="thumbnail">
+                <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+            </div>
+            <div class="blog-info">
+                <h3>Blog title</h3>
+                <p class="date">November 13, 2020</p>
+                <a class="bracket-link" href="blog-post.php">Read post</a>
+                <p>Nunc tempus pretium mauris sit amet pretium. Ut quis velit at nunc laoreet pharetra.</p>
+            </div>
+        </div>
+        
+        <div class="blog-tile">
+            <div class="thumbnail">
+                <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+            </div>
+            <div class="blog-info">
+                <h3>Blog title</h3>
+                <p class="date">November 13, 2020</p>
+                <a class="bracket-link" href="blog-post.php">Read post</a>
+                <p>Nunc tempus pretium mauris sit amet pretium. Ut quis velit at nunc laoreet pharetra.</p>
+            </div>
+        </div>
+
+        <div class="blog-tile">
+            <div class="thumbnail">
+                <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+            </div>
+            <div class="blog-info">
+                <h3>Blog title</h3>
+                <p class="date">November 13, 2020</p>
+                <a class="bracket-link" href="blog-post.php">Read post</a>
+                <p>Nunc tempus pretium mauris sit amet pretium. Ut quis velit at nunc laoreet pharetra.</p>
+            </div>
+        </div>
+
+	</section>
+
+</main>
+
+<?php include "_footer.php";?>
+<?php include "_bottom.php";?>
