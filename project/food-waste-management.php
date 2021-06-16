60330c031c037f0e3e3295ac62bd921ae6fffa4d
georgiabains <georgiabains@hotmail.co.uk>
2021-06-16 16:02:49 -0400

    Wrote project outline/description for Food Waste Management App.

--- project/food-waste-management.php
+++ project/food-waste-management.php
@@ -21,7 +21,7 @@
 
 	<section class="project-page-details">
 
-		<h2>Website Title</h2>
+		<h2>Food Waste Management App</h2>
         <h3>> mobile app<span class="blinking-underscore">_</span></h3>
 
         <div class="project-basic-info">
@@ -41,20 +41,33 @@
 
         <figure>
             <img src="../img/projects/food_waste_management/food_waste_management_mockup_one.png">
-            <figcaption>Lorem ipsum.</figcaption>
+            <figcaption>Screenshots of 9 app pages as mockups on iPhone 11s.</figcaption>
         </figure>
 
         <p class="description">
-            Lorem ipsum.
+            I created this mobile app design as part of a group project in University of Toronto's CSC318: The Design of Interactive Computational Media course. Our team consisted of myself, Isabella Kuo, Alex Waddell, Faiyaz Huq, and Mohammad Ettouhami and our group, Psyched, was one of six teams (out of the class total of 20), which the instructor and TAs favoured, based on a critieria which included creativity, protoype design, and the quality of written reports.
+            <br><br>
+            The project's assignment was to create a digital solution to a non-digital problem that people faced in their day-to-day activities. We were tasked with brainstorming various problems and deciding, as a team, which problem we'd like to explore. We ultimately decided on tackling the issue of food waste among young adults, using a mobile app to facilitate food management.
         </p>
 
         <figure>
             <img src="../img/projects/food_waste_management/food_waste_management_mockup_two.png">
-            <figcaption>Lorem ipsum.</figcaption>
+            <figcaption>Screenshots of the Add New Item popup, Inventory page, and Recipes page on iPhone 12s.</figcaption>
         </figure>
 
         <p class="description">
-            Lorem ipsum.
+            Our solution was to design a mobile app which tracks the contents of an individual's fridge and pantry, and notifies the user when a food item is nearing its expiration date. In addition, we designed a Recipes page to provide inspiration for certain food items and a Donations map to help a user locate local food banks. I was responsible for designing the Recipes page.
+        </p>
+
+        <figure>
+            <img src="../img/projects/food_waste_management/food_waste_management_storyboard.png">
+            <figcaption>Two storyboards of our app which demonstrate the basic goals of our app, and its intended positive effect on the user. Left: me (Georgia Bains). Right: Isabella Kuo.</figcaption>
+        </figure>
+
+        <p class="description">
+            While our high-fidelity prototype was constructed in Figma, the scope of the project involved conducting User Research questionnaires and interviews on our target audience (young adults) as well as industry professionals (food bank and grocery store employees). We used a combination of online social media (Reddit, Instagram) and networking (friends-of-friends) to collect our data.
+            <br><br>
+            We used our research and a number of design principles learned throughout the class, such as Jakob Nielen's Heuristics, to brainstorm, storyboard and paper prototype our final design.
         </p>
 
         <div class="project-page-slider">
@@ -77,12 +90,12 @@
                     <button class="play"><i class="fa fa-play"></i> Play</button>
                     <button class="pause"><i class="fa fa-pause"></i> Pause</button>
                 </div>
-                <p class="slider-caption">Lorem ipsum</p>
+                <p class="slider-caption">A slideshow which depicts two screenshots of Figma's interface: the Design and Prototype, specifically focused on the Recipes pages and interactions.</p>
             </div>
         </div>
 
         <p class="description">
-            Lorem ipsum <code>code test</code>.
+            The slideshow above demonstrates most of my work in Figma, where I used hand-drawn designs created by Isabella and Faiyaz at earlier stages of the project as a guide for the digital final product. We decided on using Figma for our final prototype, as it allowed us to work on our specific pages in our own time, while facilitating app-wide page links and transitions.
         </p>
 
         <figure>
@@ -94,9 +107,13 @@
                     <button class="play"><i class="fa fa-play"></i> Play</button>
                     <button class="pause"><i class="fa fa-pause"></i> Pause</button>
                 </div>
-                <figcaption class="video-caption">Lorem ipsum.</figcaption>
+                <figcaption class="video-caption">A recording of the Recipes section of the app within Figma, showcasing button interactions and page transitions.</figcaption>
             </div>
         </figure>
+
+        <p class="description">
+            This assignment was quite novel as it was focused on developing our skills in conducting user research, and on using design principles to construct a working prototype for managing food waste. Even though the course was part of the computer science program, there was no coding involved, as the focus was on the thought and intent behind every design choice, and working collaboratively to create a solution to a problem.
+        </p>
 
         <a href="../projects.php" class="bracket-link">view all projects</a>
 
